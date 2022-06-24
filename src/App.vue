27a85a0c615b9c5f0e9ef00553ad5c292ba9cba9
Nathan <novadrake76@gmail.com>
2022-06-24 17:24:42 -0300

    first look

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <br>
+  <router-link to="/">Home</router-link> | 
+  <router-link to="/about">About</router-link> | 
+  <router-view></router-view>
+</template>
+
+<script>
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
